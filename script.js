906a430f57e835189c881c21244944c94a4671cf
amnaajmal <38943088+amnaajmal@users.noreply.github.com>
2018-08-28 14:03:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -9,29 +9,14 @@ function getWeather(){
    success:function(data){
 
 console.log(data);
-// const isNight;
-// if (data.list[1].weather[0].main===rain) {
-// console.log(document.querySelector("body").style.background-color="skyblue");
-
-
-// }
-
-
-
 document.querySelector(".city-name").innerHTML=data.city.name;
-document.querySelector(".date").innerHTML=data.list[4].dt_txt;
+document.querySelector(".date").innerHTML=data.list[0].dt_txt;
 
 document.querySelector(".temprature > span").innerHTML=data.list[0].main.temp;
-document.querySelector(".discription").innerHTML=data.list[1].weather[0].main;
+document.querySelector(".discription").innerHTML=data.list[0].weather[0].main;
 document.querySelector(".min").innerHTML=data.list[0].main.temp_min;
 document.querySelector(".max").innerHTML=data.list[0].main.temp_max;
 document.querySelector(".hum >span").innerHTML=data.list[0].main.humidity;
-
-
-
-
-
-
    },
 
 	   error:function(error){
