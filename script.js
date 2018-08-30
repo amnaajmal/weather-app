dc5196ca690c96830e8bcefba45e776de844b5c7
amnaajmal <38943088+amnaajmal@users.noreply.github.com>
2018-08-30 17:10:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,33 @@ document.querySelector(".min").innerHTML=data.list[0].main.temp_min;
 document.querySelector(".max").innerHTML=data.list[0].main.temp_max;
 document.querySelector(".hum >span").innerHTML=data.list[0].main.humidity;
    },
+		let iconName = data.list[0].weather[0].main;
+			if (iconName === `Clouds`) {
+				document.querySelector(".icon").innerHTML = `<i class="wi wi-cloud"></i>`;
+				document.body.style.backgroundImage = "url('http://4.bp.blogspot.com/-1BmzmaRJ-_c/UVfQLT66iqI/AAAAAAAACaE/uJbILiIvMx0/s1600/sky+cloud+wallpapers+hd+(3).png')";
+
+			} else if (iconName === `Rain`) {
+				document.querySelector(".icon").innerHTML = `<i class="wi wi-rain"></i>`;
+				document.body.style.backgroundImage = "url('https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/D8qa-2E/rainy-stormy-darkness-wet-weather-background-depressed-sad-background_bzxjr4x7h__F0000.png')";
+			}else if (iconName === `Snow`) {
+				document.querySelector(".icon").innerHTML = `<i class="wi wi-snow"></i>`;
+				document.body.style.backgroundImage = "url('https://i.ytimg.com/vi/r6VkCdQQdG0/maxresdefault.jpg')";
+				document.body.style.color="white";
+			} else if (iconName === `Sunny`) {
+				document.querySelector(".icon").innerHTML = `<i class="wi wi--day-sunny"></i>`;
+				document.body.style.backgroundImage = "url('http://images6.fanpop.com/image/photos/36600000/Sun-star-image-sun-star-36682661-1920-1080.jpg')";
+			} else if (iconName === `Haze`) {
+				document.querySelector(".icon").innerHTML = `<i class="wi wi-day-haze"></i>`;
+				document.body.style.backgroundImage = "url('https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/nz06K7R/dark-scary-lake-at-night-background-loop-raw-1-white-haze-floating-over-the-still-lake-surrounded-by-a-creepy-forest-seamless-looping-perfect-for-backgrounds-backdrops-intro-outro-or-credits-for-themes-like-halloween-apocalypse_v1-sfrwh__F0000.png')";
+
+			} else if (iconName === `Clear`) {
+				document.querySelector(".icon").innerHTML = `<i class="wi wi-night-clear"></i>`;
+				document.body.style.backgroundImage = "url('https://jooinn.com/images/blue-sky-99.jpg')";
+
+			}
+			else {
+				document.querySelector(".icon").innerHTML = `Loading icon.....`;
+			}
 
 	   error:function(error){
         console.log(error);
